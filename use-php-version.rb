63a3b3aa0d5d035dc8fbeafb0de6df7e44b04dd3
Salajan Silviu <salanoid@gmail.com>
2022-08-23 15:48:03 +0300

    if version is not installed, install and switch to
    if version is already installed will be implemented in future

--- use-php-version.rb
+++ use-php-version.rb
@@ -11,16 +11,23 @@ current_version = get_current_version.match(/[PHP]?\s*\K[\d\.]+/)[0]
 
 puts "#{current_version} is already in use" if current_version == version
 return if current_version == version
-    
+
 `systemctl restart apache2`
-`apt install php#{version}`
-`apt install php#{version}-common php#{version}-mysql php#{version}-xml php#{version}-xmlrpc php#{version}-curl php#{version}-gd php#{version}-imagick php#{version}-cli php#{version}-dev php#{version}-imap php#{version}-mbstring php#{version}-opcache php#{version}-soap php#{version}-zip php#{version}-intl -y`
-`a2dismod php#{current_version}`
-`a2enmod php#{version}`
-`service apache2 restart`
-`update-alternatives --set php /usr/bin/php#{version}`
-`update-alternatives --set phar /usr/bin/phar#{version}`
-`systemctl restart apache2`
-`update-alternatives --config php`
-`systemctl restart apache2`
-puts "Swiched from #{current_version} to #{version}."
+
+if `apt list --installed | grep php`.include?(version)
+  puts "Version already installed."
+  return 
+else
+  `apt install php#{version}`
+  `apt install php#{version}-common php#{version}-mysql php#{version}-xml php#{version}-xmlrpc php#{version}-curl php#{version}-gd php#{version}-imagick php#{version}-cli php#{version}-dev php#{version}-imap php#{version}-mbstring php#{version}-opcache php#{version}-soap php#{version}-zip php#{version}-intl -y`
+  # end
+  `a2dismod php#{current_version}`
+  `a2enmod php#{version}`
+  `service apache2 restart`
+  `update-alternatives --set php /usr/bin/php#{version}`
+  `update-alternatives --set phar /usr/bin/phar#{version}`
+  `systemctl restart apache2`
+  `update-alternatives --config php`
+  `systemctl restart apache2`
+  puts "Swiched from #{current_version} to #{version}."
+end
